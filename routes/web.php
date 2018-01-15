ae7965346d4674212aa700f49d5f478ad41a4bab
Jose Domenech Leal <josesguay@gmail.com>
2018-01-15 18:18:57 +0100

    Ruta para devolver ficheros compilados Angular

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return File::get(public_path('index.html'));
 });
 
 Route::get('user/verify/{verification_code}', 'AuthController@verifyUser');
