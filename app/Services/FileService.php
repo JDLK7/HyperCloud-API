4842fa9cd825b5870f89e7d1a2690174dcd0535e
Jose Domenech Leal <josesguay@gmail.com>
2018-01-15 18:17:50 +0100

    Método de servicio para crear archivos

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Folder;
+use App\Archive;
 
 class FileService 
 {
@@ -10,8 +11,8 @@ class FileService
      * Crea una carpeta una nueva carpeta dentro de la carpeta $parent.
      *
      * @param string $name
-     * @param Folder $parent
-     * @return Folder
+     * @param \App\Folder $parent
+     * @return \App\Folder
      */
     public function createFolder($name, Folder $parent) {
         $path = $parent->path . $name . "/";
@@ -32,5 +33,36 @@ class FileService
         $folder->save();
 
         return $folder;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param string $name
+     * @param string $extension
+     * @param int $size
+     * @param \App\Folder $parent
+     * @return \App\Archive
+     */
+    public function createArchive($name, $extension, $size, Folder $parent) {
+        $path = $parent->path . $name;
+
+        $exists = Archive::where('path', $path)->exists();
+
+        if($exists) {
+            throw new FileServiceException('No se ha podido subir el archivo porque ya existe');
+        }
+
+        $archive = Archive::create([
+            'name'      => $name,
+            'path'      => $path,
+            'extension' => $extension,
+            'size'      => $size,
+        ]);
+
+        $archive->folder()->associate($parent);
+        $archive->save();
+
+        return $archive;
     }
 }
